e4a3d647f53e39004116b5d3837107fdb32a74c5
gamma0987 <gamma0987@posteo.de>
2026-09-01 05:44:15 +0200

    lint(clippy): resort types following new clippy rules in valgrind-requests

--- crates/valgrind-requests-tests/tests/common/mod.rs
+++ crates/valgrind-requests-tests/tests/common/mod.rs
@@ -8,9 +8,9 @@ use assert_cmd::assert::{Assert, AssertError};
 use tempfile::{TempDir, tempdir};
 use version_compare::Cmp;
 
-pub const VALGRIND_WRAPPER: &str = env!("CARGO_BIN_EXE_valgrind-wrapper");
 pub const FIXTURES_DIR: &str = env!("VALGRIND_REQUESTS_TESTS_FIXTURES");
 pub const RUST_VERSION: &str = env!("VALGRIND_REQUESTS_TESTS_RUST_VERSION");
+pub const VALGRIND_WRAPPER: &str = env!("CARGO_BIN_EXE_valgrind-wrapper");
 
 #[derive(Debug, Clone)]
 pub enum Matcher {
@@ -52,6 +52,11 @@ impl Matcher {
     }
 }
 
+pub fn compare_rust_version(cmp: Cmp, expected: &str) -> bool {
+    version_compare::compare_to(get_rust_version(), expected, cmp)
+        .expect("Rust version comparison should succeed")
+}
+
 fn find_runner() -> Option<String> {
     for (key, value) in std::env::vars() {
         if key.starts_with("CARGO_TARGET_") && key.ends_with("_RUNNER") && !value.is_empty() {
@@ -59,19 +64,6 @@ fn find_runner() -> Option<String> {
         }
     }
     None
-}
-
-pub fn get_rust_version() -> String {
-    RUST_VERSION.to_owned()
-}
-
-pub fn compare_rust_version(cmp: Cmp, expected: &str) -> bool {
-    version_compare::compare_to(get_rust_version(), expected, cmp)
-        .expect("Rust version comparison should succeed")
-}
-
-pub fn get_test_bin_path(name: &str) -> PathBuf {
-    PathBuf::from(VALGRIND_WRAPPER).parent().unwrap().join(name)
 }
 
 pub fn get_command<T>(path: T) -> Command
@@ -91,30 +83,6 @@ where
     }
 }
 
-pub fn get_test_bin_command<T>(name: T) -> Command
-where
-    T: AsRef<str>,
-{
-    let path = PathBuf::from(VALGRIND_WRAPPER)
-        .parent()
-        .unwrap()
-        .join(name.as_ref());
-    get_command(path)
-}
-
-pub fn get_valgrind_wrapper_command() -> Command {
-    Command::new(VALGRIND_WRAPPER)
-}
-
-pub fn get_fixture_path(name: &str) -> PathBuf {
-    PathBuf::from(FIXTURES_DIR).join(name)
-}
-
-pub fn get_fixture_as_string(name: &str) -> String {
-    fs::read_to_string(get_fixture_path(name))
-        .unwrap_or_else(|_| panic!("Reading fixture '{name}' should succeed"))
-}
-
 pub fn get_fixture(name: &str, target: Option<&str>, since: Option<&str>, suffix: &str) -> String {
     let mut file_name = String::from(name);
     if let Some(since) = since {
@@ -127,6 +95,38 @@ pub fn get_fixture(name: &str, target: Option<&str>, since: Option<&str>, suffix
     get_fixture_as_string(&file_name)
 }
 
+pub fn get_fixture_as_string(name: &str) -> String {
+    fs::read_to_string(get_fixture_path(name))
+        .unwrap_or_else(|_| panic!("Reading fixture '{name}' should succeed"))
+}
+
+pub fn get_fixture_path(name: &str) -> PathBuf {
+    PathBuf::from(FIXTURES_DIR).join(name)
+}
+
+pub fn get_rust_version() -> String {
+    RUST_VERSION.to_owned()
+}
+
 pub fn get_sandbox() -> TempDir {
     tempdir().expect("Creating sandbox directory failed")
+}
+
+pub fn get_test_bin_command<T>(name: T) -> Command
+where
+    T: AsRef<str>,
+{
+    let path = PathBuf::from(VALGRIND_WRAPPER)
+        .parent()
+        .unwrap()
+        .join(name.as_ref());
+    get_command(path)
+}
+
+pub fn get_test_bin_path(name: &str) -> PathBuf {
+    PathBuf::from(VALGRIND_WRAPPER).parent().unwrap().join(name)
+}
+
+pub fn get_valgrind_wrapper_command() -> Command {
+    Command::new(VALGRIND_WRAPPER)
 }
